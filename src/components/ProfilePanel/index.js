c65a46bdaef0ab1ae65b7b50fde89d9b039cf47d
Andrey Los <puha212@gmail.com>
2018-05-18 20:40:53 +0300

    #7 Create ProfilePanel styles - Slight changes

--- src/components/ProfilePanel/index.js
+++ src/components/ProfilePanel/index.js
@@ -1,40 +1,15 @@
 import './index.css'
 
-const dropdownHeader = document.querySelector('.dropdown__header');
-const dropdownOpenButton = document.querySelector('.dropdown__header__openButton');
-const dropdownContent = document.querySelector('.dropdown__content');
+const dropdownHeader = document.querySelector('.dropdown__header')
+const dropdownOpenButton = document.querySelector(
+  '.dropdown__header__openButton',
+)
+const dropdownContent = document.querySelector('.dropdown__content')
 
-const openDropdown = () => {
-  if (dropdownHeader.className === 'dropdown__header') {
-    dropdownHeader.className += ' dropdown__header--open';
-    dropdownOpenButton.className += ' dropdown__header__openButton--open';
-    dropdownContent.className += ' dropdown__content--open';
-  } else {
-  dropdownHeader.className = 'dropdown__header';
-    dropdownOpenButton.className = 'dropdown__header__openButton'
-    dropdownContent.className = 'dropdown__content'
-  };
+const toggleDropdown = () => {
+  dropdownHeader.classList.toggle('dropdown__header--open')
+  dropdownOpenButton.classList.toggle('dropdown__header__openButton--open')
+  dropdownContent.classList.toggle('dropdown__content--open')
 }
 
-dropdownOpenButton.addEventListener('click', openDropdown)
-
-// const dropDownElements = {
-//   dropdownHeader: document.querySelector('.dropdown__header'),
-//   dropdownOpenButton: document.querySelector('.dropdown__header__openButton'),
-//   dropdownContent: document.querySelector('.dropdown__content'),
-// }
-
-// const openDropdown = () => {
-//   if (dropDownElements.dropdownHeader.className === 'dropdown__header'){
-//     for (let key in dropDownElements){
-//       if (dropDownElements.hasOwnProperty(key)) {
-//         console.log(dropDownElements[key])
-//         // dropDownElements[key.className] += ' ${key}--open'
-//       }
-//     }
-//   }else {
-//      console.log('LOL')
-//   }
-// }
-
-// dropDownElements.dropdownOpenButton.addEventListener('click', openDropdown)
+dropdownOpenButton.addEventListener('click', toggleDropdown)
